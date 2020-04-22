82d0463dde1823ac0949b5a7498fb4b3d83e8129
Odero Oluoch <52481094+0deroOluoch@users.noreply.github.com>
2020-04-22 04:53:00 +0300

    Add files via upload

---
+++ IfThen/src/com/appnetics/Main.java
@@ -0,0 +1,16 @@
+package com.appnetics;
+
+public class Main {
+
+    public static void main(String[] args) {
+	boolean isCar = false;
+	if (isCar) {
+        System.out.println("am confused a little here");
+    }
+
+
+	boolean wasCar = isCar ? true : false;
+
+    }
+}
+
